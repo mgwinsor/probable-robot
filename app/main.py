5de2d4c287df68aa083408a18141fde5fec1ba3f
Michael Winsor <mgwinsor@gmail.com>
2025-01-05 15:20:16 +0800

    fix: build system and entry point

--- app/main.py
+++ app/main.py
@@ -2,12 +2,8 @@ import typer
 
 import app.cli.asset as cli_asset
 
-
-def main():
-    app = typer.Typer()
-    app.add_typer(cli_asset.app, name="asset")
-    app()
-
+app = typer.Typer()
+app.add_typer(cli_asset.app, name="asset")
 
 if __name__ == "__main__":
-    main()
+    app()
