93411c0bfa83dd84e6f67f3635b7a47f34815f3f
Michael Winsor <mgwinsor@gmail.com>
2025-01-05 15:19:22 +0800

    add: pytest-cov and unique asset test

--- tests/test_cli_asset.py
+++ tests/test_cli_asset.py
@@ -1,11 +1,17 @@
 from typer.testing import CliRunner
 
-from app.cli.asset import app
+from app.main import app
 
 runner = CliRunner()
 
 
 def test_asset_add():
-    result = runner.invoke(app, ["add", "BTC", "Bitcoin", "crytpo"])
+    result = runner.invoke(app, ["asset", "add", "BTC", "Bitcoin", "crytpo"])
     assert result.exit_code == 0
     assert "Added Bitcoin to your asset list!" in result.stdout
+
+
+def test_asset_unique_add():
+    _ = runner.invoke(app, ["asset", "add", "BTC", "Bitcoin", "crypto"])
+    result = runner.invoke(app, ["asset", "add", "BTC", "Bitcoin", "crypto"])
+    assert result.exit_code == 1
