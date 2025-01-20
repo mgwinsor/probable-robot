4210c9d7c0cd07bb1d9c337584cad7d9c9a01ddc
Michael Winsor <mgwinsor@gmail.com>
2025-01-20 23:44:49 +0800

    update: asset table definition

---
+++ app/models/asset.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models import Base
+
+if TYPE_CHECKING:
+    from app.models import (IncomeEvent, PurchaseLot, SalesPl, TransferFeePl,
+                            WalletTransfer)
+
+
+class Asset(Base):
+    __tablename__ = "assets"
+
+    asset_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    symbol: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    name: Mapped[Optional[str]] = mapped_column(String)
+    decimal_places: Mapped[int] = mapped_column(Integer, nullable=False)
+    current_unit_price: Mapped[int] = mapped_column(Integer)
+
+    purchase_lots: Mapped[list["PurchaseLot"]] = relationship(back_populates="asset")
+    income_events: Mapped[list["IncomeEvent"]] = relationship(back_populates="asset")
+    sales: Mapped[list["SalesPl"]] = relationship(back_populates="asset")
+    transfers: Mapped[list["WalletTransfer"]] = relationship(back_populates="asset")
+    transfer_fees: Mapped[list["TransferFeePl"]] = relationship(back_populates="asset")
